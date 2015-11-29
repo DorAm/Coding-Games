c82bdefe6242cb17e9b27a7e081a84ed0f6d1f99
Dor Amar <bndor.am@gmail.com>
2015-11-29 16:36:48 +0200

    js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,12 @@
 
 $.getJSON('../server/games.json', function(data){
-    console.log(data);
+    var htmlString = '<ul class="gamesList">';
+    $.each(data, function(key, val){
+        htmlString += '<li>';
+        htmlString += '<h3>' + val.name +'</h2>';
+        htmlString += '</li>';
+    });
+    htmlString += '</ul>';
+    $('#gamesArea').html(htmlString);
+
 }); //get JSON
