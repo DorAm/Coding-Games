1e262944abe2616ecdc519de4f220b1758391351
uzigi <shlakim2010>
2015-12-01 14:45:02 +0200

    contact json created

--- public/js/contactUs.js
+++ public/js/contactUs.js
@@ -1,1 +1,11 @@
 
+function sendForm (elForm){
+    $.ajax({
+        url: '../server/data/contact',
+        data:{name: elForm[0].value, } ,
+        type: 'POST',
+        success: function(elForm) {
+           console.log('elForm');
+        }
+    });
+}
