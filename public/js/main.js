875066774be55f8ebca02e7cb19a2578aee15d30
uzigi <shlakim2010>
2015-12-01 10:57:36 +0200

    adds contact html, css, and js

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,23 @@
 $.getJSON('api/games/', function(data){
 
     console.log(data);
-    var htmlString = '<ul class="gamesList">';
+    //var htmlString = '<ul class="gamesList">';
+    var htmlString = '';
     $.each(data, function(key, val){
-        htmlString += '<li>';
+        //htmlString += '<h3>' + val.name +'</h3></br>';
+        //htmlString += '<li class="child child-' + key + ' ' + val.id+'"><img src="../img/'+val.id+'.png">';
+        //htmlString += '</li>';
         htmlString += '<h3>' + val.name +'</h3>';
-        htmlString += '</li>';
+        htmlString += '<div class="child child-' + key + ' ' + val.id+'"><img src="../img/'+val.id+'.png">';
+        htmlString += '</div>';
+
     });
     htmlString += '</ul>';
     $('.gamesArea').html(htmlString);
 
 });
 
+$(".nav-toggle").on('click', function (e) {
+    $("nav").toggleClass("show");
+    e.preventDefault();
+});
