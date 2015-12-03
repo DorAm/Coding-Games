35a8c645100cd2153c19fdfc34d0fce591bdd306
uzigi <shlakim2010>
2015-12-03 10:53:03 +0200

    something

--- public/js/contactUs.js
+++ public/js/contactUs.js
@@ -1,10 +1,16 @@
+
 function sendForm(elForm) {
     $.ajax({
         url: 'api/contact/',
         data: {name: elForm[0].value, Email: elForm[1].value, subject: elForm[2].value, message: elForm[3].value},
         type: 'POST',
         success: function () {
-            console.log('success');
+            $('.contact-form').trigger("reset");
+            $('.confirmessage').fadeIn(1000);
+            setTimeout(function (){
+                $('.confirmessage').fadeOut(1000);
+            }, 2000)
+
         }
     });
 }
